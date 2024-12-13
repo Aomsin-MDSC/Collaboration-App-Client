9ba21c51da3f85f1b9576054a4cc07a7b4a6b3f9
Full-Stack-boi <151743553+Full-Stack-boi@users.noreply.github.com>
2024-12-13 13:04:31 +0700

    Authentication Controllers template
    - Login Controller (ยังไม่เสร็จ)
    - Register Controller (ยังไม่เสร็จ)

---
+++ lib/controllers/login_controller.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+
+class LoginController extends GetxController {
+  static LoginController get instance => Get.find();
+
+  final username = TextEditingController();
+  final password = TextEditingController();
+
+ //Login Func
+
+}
---
+++ lib/controllers/register_controller.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+
+class RegisterController extends GetxController {
+  static RegisterController get instance => Get.find();
+
+  final username = TextEditingController();
+  final password = TextEditingController();
+
+//Register Func
+
+}
