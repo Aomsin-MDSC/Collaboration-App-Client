868d5fa42b1f806e7ea674f047a1d38577bf3d4e
Full-Stack-boi <151743553+Full-Stack-boi@users.noreply.github.com>
2024-12-24 13:52:30 +0700

    Project View
    - เปลี่ยน Switch ให้มี text บอก User
    - ทำ Carousel เพื่อที่จะแสดง Announce
    - + Search bar

---
+++ lib/views/project_assets/project_form.dart
@@ -0,0 +1,206 @@
+import 'package:animated_toggle_switch/animated_toggle_switch.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:collaboration_app_client/controllers/new_task_controller.dart';
+import 'package:collaboration_app_client/controllers/project_controller.dart';
+import 'package:collaboration_app_client/views/edit_task_view.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
+
+class ProjectForm extends StatefulWidget {
+  const ProjectForm({super.key});
+
+  @override
+  State<ProjectForm> createState() => _ProjectFormState();
+}
+
+class _ProjectFormState extends State<ProjectForm> {
+  late final NewTaskController newTaskController;
+  final Map<int, bool> toggleStates = {}; // Track toggle states by index
+  String searchQuery = ''; // Track the search query
+  bool isSearchVisible = false; // To control the visibility of the search bar
+  // final TextEditingController _searchController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    Get.put(ProjectController());
+    newTaskController = Get.put(NewTaskController());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final List<String> announce = [
+      "Announce 1",
+      "Announce 2",
+      "Announce 3",
+      "Announce 4",
+      "Announce 5",
+      "Announce 6",
+      "Announce 7",
+      "Announce 8",
+      "Announce 9",
+      "Announce 10",
+    ];
+
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        // Conditionally show the search bar with animation
+        CarouselSlider(
+          options: CarouselOptions(
+              height: 100,
+              autoPlay: true,
+              autoPlayInterval: const Duration(seconds: 3)),
+          items: announce.map((i) {
+            return Builder(
+              builder: (BuildContext context) {
+                return SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  child: Card(
+                      margin: const EdgeInsets.symmetric(horizontal: 5.0),
+                      child: Text(
+                        i,
+                        style: const TextStyle(fontSize: 16.0),
+                        textAlign: TextAlign.center,
+                      )),
+                );
+              },
+            );
+          }).toList(),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 20, left: 15, right: 15),
+          child: TextField(
+            decoration: InputDecoration(
+              labelText: 'Search',
+              prefixIcon: const Icon(Icons.search),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+            onChanged: (value) {
+              setState(() {
+                searchQuery = value.toLowerCase(); // Update search query
+              });
+            },
+          ),
+        ),
+        const SizedBox(height: 10),
+        Expanded(
+          child: Obx(() {
+            final filteredList = newTaskController.taskList.where((task) {
+              return task.taskName!.toLowerCase().contains(searchQuery);
+            }).toList();
+
+            return RefreshIndicator(
+              onRefresh: () async {
+                newTaskController.taskList.refresh();
+              },
+              child: ReorderableListView.builder(
+                padding: EdgeInsets.only(bottom: 120),
+                shrinkWrap: true,
+                itemCount: filteredList.length,
+                itemBuilder: (context, index) {
+                  final taskList = filteredList[index];
+
+                  return TextButton(
+                      key: ValueKey(taskList),
+                      onPressed: () {
+                        // Api Here
+                        Get.to(const EditTaskView());
+                      },
+                      child: Card(
+                        margin: const EdgeInsets.all(0),
+                        child: ListTile(
+                          title: Text(taskList.taskName!),
+                          subtitle: Text(taskList.taskName!),
+                          trailing: AnimatedToggleSwitch<bool>.dual(
+                            indicatorSize: const Size.fromWidth(40),
+                            height: context.height * 0.053,
+                            current: taskList.isToggled,
+                            first: false,
+                            second: true,
+                            style: const ToggleStyle(
+                              borderColor: Colors.transparent,
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.black26,
+                                  spreadRadius: 1,
+                                  blurRadius: 2,
+                                  offset: Offset(0, 2),
+                                ),
+                              ],
+                            ),
+                            onChanged: (value) {
+                              // API Here
+                              setState(() {
+                                taskList.isToggled = value;
+                              });
+                            },
+                            textBuilder: (value) => value
+                                ? const Center(
+                                    child: Text(
+                                      'Done',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  )
+                                : const Center(
+                                    child: Text(
+                                      'Pending  ',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ),
+                            styleBuilder: (value) => value
+                                ? ToggleStyle(
+                                    backgroundColor: Colors.green,
+                                    indicatorColor: Colors.white,
+                                    indicatorBorder: Border.all(
+                                      color: Colors.green,
+                                      width: 3,
+                                    ))
+                                : const ToggleStyle(
+                                    backgroundColor: Colors.redAccent,
+                                    indicatorColor: Colors.white,
+                                  ),
+                          ),
+                        ),
+                      ));
+                },
+                proxyDecorator:
+                    (Widget child, int index, Animation<double> animation) {
+                  return Material(
+                    child: DecoratedBox(
+                      decoration: BoxDecoration(
+                        color: Colors.grey.withOpacity(0.5),
+                        borderRadius: BorderRadius.circular(18),
+                      ),
+                      child: child,
+                    ),
+                  );
+                },
+                onReorder: (int oldIndex, int newIndex) {
+                  if (newIndex > oldIndex) {
+                    newIndex -= 1;
+                  }
+                  //Api Here
+                  final items = newTaskController.taskList.removeAt(oldIndex);
+                  newTaskController.taskList.insert(newIndex, items);
+                },
+              ),
+            );
+          }),
+        ),
+      ],
+    );
+  }
+}
--- lib/views/project_view.dart
+++ lib/views/project_view.dart
@@ -1,10 +1,11 @@
-import 'package:collaboration_app_client/views/edit_task_view.dart';
+import 'package:collaboration_app_client/views/edit_project_view.dart';
+import 'package:collaboration_app_client/views/new_announce_view.dart';
+import 'package:collaboration_app_client/views/project_assets/project_form.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_expandable_fab/flutter_expandable_fab.dart';
 import 'package:get/get.dart';
 import '../controllers/new_task_controller.dart';
 import '../controllers/project_controller.dart';
-import '../views/new_project_view.dart';
 import '../views/new_task_view.dart';
 
 class ProjectView extends StatefulWidget {
@@ -15,18 +16,18 @@ class ProjectView extends StatefulWidget {
 }
 
 class _ProjectViewState extends State<ProjectView> {
-  late final NewTaskController newTaskController;
-  final Map<int, bool> toggleStates = {}; // Track toggle states by index
-  String searchQuery = ''; // Track the search query
-  bool isSearchVisible = false; // To control the visibility of the search bar
-  final TextEditingController _searchController = TextEditingController();
+  // late final NewTaskController newTaskController;
+  // final Map<int, bool> toggleStates = {}; // Track toggle states by index
+  // String searchQuery = ''; // Track the search query
+  // bool isSearchVisible = false; // To control the visibility of the search bar
+  // // final TextEditingController _searchController = TextEditingController();
 
-  @override
-  void initState() {
-    super.initState();
-    Get.put(ProjectController());
-    newTaskController = Get.put(NewTaskController());
-  }
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   Get.put(ProjectController());
+  //   newTaskController = Get.put(NewTaskController());
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -35,207 +36,98 @@ class _ProjectViewState extends State<ProjectView> {
 
     return SafeArea(
       child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.amber,
-          title: const Text("Project Page"),
-        ),
-        floatingActionButtonLocation: ExpandableFab.location,
-        floatingActionButton: isKeyboardVisible
-            ? null // Hide FAB when the keyboard is visible
-            : ExpandableFab(
-                childrenAnimation: ExpandableFabAnimation.rotate,
-                distance: 117,
-                openButtonBuilder: RotateFloatingActionButtonBuilder(
-                    angle: -3.14 / 2,
-                    fabSize: ExpandableFabSize.large,
-                    child: const Icon(Icons.add)),
-                closeButtonBuilder: RotateFloatingActionButtonBuilder(
-                    angle: -3.14 / 2,
-                    fabSize: ExpandableFabSize.large,
-                    child: const Icon(Icons.close)),
-                type: ExpandableFabType.fan,
-                pos: ExpandableFabPos.center,
-                fanAngle: 180,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(bottom: 110),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Column(
-                          children: [
-                            const Text(
-                              "New Project",
-                              style: TextStyle(
-                                  fontSize: 17, fontWeight: FontWeight.bold),
-                            ),
-                            const SizedBox(height: 10),
-                            FloatingActionButton.large(
-                              heroTag: null,
-                              child: const Icon(Icons.edit),
-                              onPressed: () {
-                                Get.to(const NewProjectView());
-                              },
-                            ),
-                          ],
-                        ),
-                        const SizedBox(width: 20),
-                        Column(
-                          children: [
-                            const Text(
-                              "New Task",
-                              style: TextStyle(
-                                  fontSize: 17, fontWeight: FontWeight.bold),
-                            ),
-                            const SizedBox(height: 10),
-                            FloatingActionButton.large(
-                              heroTag: null,
-                              child: const Icon(Icons.task),
-                              onPressed: () {
-                                Get.to(const NewTaskView());
-                              },
-                            ),
-                          ],
-                        ),
-                        const SizedBox(width: 20),
-                        Column(
-                          children: [
-                            const Text(
-                              "Search",
-                              style: TextStyle(
-                                  fontSize: 17, fontWeight: FontWeight.bold),
-                            ),
-                            const SizedBox(height: 10),
-                            FloatingActionButton.large(
-                              heroTag: null,
-                              child: Icon(isSearchVisible
-                                  ? Icons.close
-                                  : Icons
-                                      .search), // Toggle between search and close icon
-                              onPressed: () {
-                                setState(() {
-                                  isSearchVisible =
-                                      !isSearchVisible; // Toggle search visibility
-                                });
-
-                                if (isSearchVisible) {
-                                  FocusScope.of(context)
-                                      .requestFocus(FocusNode());
-                                } else {
-                                  _searchController.clear();
-                                }
-                              },
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-        body: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-              // Conditionally show the search bar with animation
-              AnimatedOpacity(
-                opacity: isSearchVisible ? 1.0 : 0.0,
-                duration: const Duration(milliseconds: 300), // Shorter duration
-                child: Visibility(
-                  visible: isSearchVisible,
-                  child: Padding(
-                    padding:
-                        const EdgeInsets.only(top: 20, left: 15, right: 15),
-                    child: TextField(
-                      controller: _searchController,
-                      decoration: InputDecoration(
-                        labelText: 'Search',
-                        prefixIcon: const Icon(Icons.search),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                      ),
-                      onChanged: (value) {
-                        setState(() {
-                          searchQuery =
-                              value.toLowerCase(); // Update search query
-                        });
-                      },
-                    ),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 10),
-              Expanded(
-                child: Obx(() {
-                  final filteredList = newTaskController.taskList.where((task) {
-                    return task.taskName!.toLowerCase().contains(searchQuery);
-                  }).toList();
-
-                  return RefreshIndicator(
-                    onRefresh: () async {
-                      newTaskController.taskList.refresh();
-                    },
-                    child: ReorderableListView.builder(
-                      itemCount: filteredList.length,
-                      itemBuilder: (context, index) {
-                        final taskList = filteredList[index];
-
-                        return TextButton(
-                          key: ValueKey(taskList),
-                          onPressed: () {
-                            // Api Here
-                            Get.to(const EditTaskView());
-                          },
-                          child: Card(
-                            margin: const EdgeInsets.all(0),
-                            child: ListTile(
-                              title: Text(taskList.taskName!),
-                              subtitle: Text(taskList.taskName!),
-                              trailing: // Toggle switch is Not following the List
-                                  Switch(
-                                value: taskList.isToggled,
-                                inactiveTrackColor:
-                                    const Color.fromARGB(255, 241, 130, 130),
-                                activeColor: Colors.green,
-                                onChanged: (bool value) {
-                                  setState(() {
-                                    taskList.isToggled = value;
-                                  });
+          appBar: AppBar(
+            backgroundColor: Colors.amber,
+            title: const Text("Project Page"),
+          ),
+          floatingActionButtonLocation: ExpandableFab.location,
+          floatingActionButton: isKeyboardVisible
+              ? null // Hide FAB when the keyboard is visible
+              : ExpandableFab(
+                  childrenAnimation: ExpandableFabAnimation.rotate,
+                  distance: 125,
+                  openButtonBuilder: RotateFloatingActionButtonBuilder(
+                      angle: -3.14 / 2,
+                      fabSize: ExpandableFabSize.large,
+                      child: const Icon(Icons.add)),
+                  closeButtonBuilder: RotateFloatingActionButtonBuilder(
+                      angle: -3.14 / 2,
+                      fabSize: ExpandableFabSize.large,
+                      child: const Icon(Icons.close)),
+                  type: ExpandableFabType.fan,
+                  pos: ExpandableFabPos.center,
+                  fanAngle: 180,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 110),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Column(
+                            children: [
+                              const Text(
+                                "Edit Project",
+                                style: TextStyle(
+                                    fontSize: 17, fontWeight: FontWeight.bold),
+                              ),
+                              const SizedBox(height: 10),
+                              FloatingActionButton.large(
+                                heroTag: null,
+                                child: const Icon(Icons.edit),
+                                onPressed: () {
+                                  Get.to(const EditProjectView());
                                 },
                               ),
-                            ),
+                            ],
                           ),
-                        );
-                      },
-                      proxyDecorator: (Widget child, int index,
-                          Animation<double> animation) {
-                        return Material(
-                          child: DecoratedBox(
-                            decoration: BoxDecoration(
-                              color: Colors.grey.withOpacity(0.5),
-                              borderRadius: BorderRadius.circular(18),
-                            ),
-                            child: child,
+                          const SizedBox(width: 20),
+                          Column(
+                            children: [
+                              const Text(
+                                "New Task",
+                                style: TextStyle(
+                                    fontSize: 17, fontWeight: FontWeight.bold),
+                              ),
+                              const SizedBox(height: 10),
+                              FloatingActionButton.large(
+                                heroTag: null,
+                                child: const Icon(Icons.task),
+                                onPressed: () {
+                                  Get.to(const NewTaskView());
+                                },
+                              ),
+                            ],
                           ),
-                        );
-                      },
-                      onReorder: (int oldIndex, int newIndex) {
-                        if (newIndex > oldIndex) {
-                          newIndex -= 1;
-                        }
-                        final items =
-                            newTaskController.taskList.removeAt(oldIndex);
-                        newTaskController.taskList.insert(newIndex, items);
-                      },
+                          const SizedBox(width: 10),
+                          Column(
+                            children: [
+                              const Text(
+                                "New Announce",
+                                style: TextStyle(
+                                    fontSize: 17, fontWeight: FontWeight.bold),
+                              ),
+                              const SizedBox(height: 10),
+                              FloatingActionButton.large(
+                                heroTag: null,
+                                child: const Icon(Icons
+                                    .announcement), // Toggle between search and close icon
+                                onPressed: () {
+                                  Get.to(const NewAnnounceView());
+                                },
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
                     ),
-                  );
-                }),
-              ),
-            ],
-          ),
-        ),
-      ),
+                  ],
+                ),
+          body: const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Column(
+              children: [Flexible(child: ProjectForm())],
+            ),
+          )),
     );
   }
 }
